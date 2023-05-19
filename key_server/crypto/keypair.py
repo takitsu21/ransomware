d396da49265c2f257ce82993dfd67b8814ac3780
takitsu21 <dylannbatisse@gmail.com>
2023-05-19 17:26:43 +0200

    refactor

--- key_server/crypto/keypair.py
+++ key_server/crypto/keypair.py
@@ -1,8 +1,5 @@
 import logging
 from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Util.Padding import pad
 
 
 logger = logging.getLogger("key_server")
@@ -34,26 +31,6 @@ class KeyPair:
         self._public_key = None
         self._private_key = None
         self.keypair = self._generate_rsa_key()
-        self.aes_key = get_random_bytes(
-            int(kwargs.get("aes_key_size", 16)))
-        self.enc_aes_key = None
-        self._prepare_cipher(
-            int(kwargs.get(
-                "aes_mode",
-                AES.MODE_CBC)))
-
-    def _prepare_cipher(self, aes_mode: int):
-        """
-        Prepare the cipher
-
-        :param aes_mode: The AES mode to use
-        """
-        self.cipher_rsa = PKCS1_OAEP.new(self.keypair)
-        self.cipher_aes = AES.new(self.aes_key,
-                                  aes_mode)
-
-        # Encrypt the session key with the public RSA key
-        self.enc_aes_key = self.cipher_rsa.encrypt(self.aes_key)
 
     def __repr__(self) -> str:
         return f"RSAAESEncryption(rsa_key_size={self.rsa_key_size})"
